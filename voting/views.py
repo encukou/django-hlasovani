bef1a343c3e216494c51484a0b37ba1c1b51cdd8
Petr Viktorin <encukou@gmail.com>
2016-04-13 00:14:58 +0200

    Implement voting

--- voting/views.py
+++ voting/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.db import transaction
 
-from voting.models import Poll
+from voting.models import Poll, Record, Vote
 
 
 def poll_list(request):
@@ -10,4 +11,35 @@ def poll_list(request):
 
 def poll_detail(request, pk):
     poll = get_object_or_404(Poll, pk=pk)
-    return render(request, 'voting/poll_detail.html', {'poll': poll})
+    error = ''
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        option_values = []
+        for option in poll.options.all():
+            if 'opt-{}'.format(option.pk) in request.POST:
+                value = True
+            else:
+                value = False
+            option_values.append((option, value))
+
+        if title:
+            with transaction.atomic():
+                record = Record(poll=poll, title=title)
+                record.save()
+                for option, value in option_values:
+                    vote = Vote(option=option, record=record, value=value)
+                    vote.save()
+            return redirect('poll_detail', pk=pk)
+        else:
+            error = 'Musíš zadat jméno.'
+    else:
+        option_values = []
+        for option in poll.options.all():
+            option_values.append((option, False))
+    data = {
+        'poll': poll,
+        'error': error,
+        'option_values': option_values,
+    }
+    print(option_values)
+    return render(request, 'voting/poll_detail.html', data)
