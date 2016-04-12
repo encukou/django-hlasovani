6eeebc4b410132d56eef772af24e9cbbc70c0176
Petr Viktorin <encukou@gmail.com>
2016-04-12 21:42:48 +0200

    Register models with admin

--- voting/admin.py
+++ voting/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from voting import models
 
 # Register your models here.
+admin.site.register(models.Poll)
+admin.site.register(models.Option)
+admin.site.register(models.Record)
+admin.site.register(models.Vote)
